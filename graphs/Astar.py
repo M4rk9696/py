ff9e86ac3bc2cafb63b0c5a5da69a2f531618c0a
nimalan <nimalan854@gmail.com>
2017-05-27 12:37:41 +0530

    Removing main

--- graphs/Astar.py
+++ graphs/Astar.py
@@ -1,3 +1,4 @@
+#Astar for matrix
 import heapq
 
 class PriorityQueue:
@@ -30,6 +31,7 @@ class Graph:
 	def cost(self, node, to):
 		return self.WEdges[node][to]
 
+# Using manhatten distance as heurtric function
 def hfunction(pta, ptb):
 	(x1, y1) = pta
 	(x2, y2) = ptb
@@ -58,54 +60,3 @@ def a_star(graph, start, goal):
 				frontier.put(next, priority)
 				came_from[next] = current
 	return cost
-
-def main():
-	inf = 100000000
-	fname = 'p083_matrix.txt'
-	with open(fname) as f:
-		content = f.readlines()
-	content = [x.strip().split(',') for x in content]
-	n = len(content)
-	content = [[inf] * (n + 2)] + [[inf] + [int(a) for a in x] + [inf] for x in content] + [[inf] * (n + 2)]
-	n = len(content)
-
-	graph = Graph()
-
-	for i in range(1, n-1):
-		for j in range(1, n-1):
-			pos = (i, j)
-			graph.weight[pos] = content[i][j]
-			## top
-			ii, jj = i - 1, j
-			node = (ii, jj)
-			weight = content[ii][jj]
-			graph.WEdges[pos] = {}
-			graph.WEdges[pos][node] = weight
-
-			## botton
-			ii, jj = i + 1, j
-			node = (ii, jj)
-			weight = content[ii][jj]
-			graph.WEdges[pos][node] = weight
-
-			## left
-			ii, jj = i, j - 1
-			node = (ii, jj)
-			weight = content[ii][jj]
-			graph.WEdges[pos][node] = weight
-
-			## right
-			ii, jj = i, j + 1
-			node = (ii, jj)
-			weight = content[ii][jj]
-			graph.WEdges[pos][node] = weight
-			
-	# print graph.WEdges
-	# print graph.neighbors((5, 5))
-	# print graph.cost((1, 1), (1, 2))
-	st = (1, 1)
-	en = (n - 2, n - 2)
-	print a_star(graph, st, en)[en]
-	# print content
-
-main()
